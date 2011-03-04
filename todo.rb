353e83c9099071285a2a124a067176853b2cf06b
Jan Andersson <jan.andersson@travelpartner.se>
2011-03-04 17:33:56 +0100

    Updated help message

--- todo.rb
+++ todo.rb
@@ -6,17 +6,17 @@ Synopsis
   Very simple todo CLI app
 
 Usage
-  todo.rb file a|add [args...]
+  todo.rb file a|add [text]
   Add todo. Run without arguments to create todos from STDIN, one per line
-
-  todo.rb file e|edit
-  Open file in editor
-
-  todo.rb file do args
-  Complete item, row numbers as arguments
 
   todo.rb file [filters...]
   List in alphanumerical order with row numbers, optional filter on arguments
+
+  todo.rb file do line...
+  Complete item, row numbers as arguments
+
+  todo.rb file e|edit
+  Open file in editor
 
   todo.rb h|help
   Displays this help message
